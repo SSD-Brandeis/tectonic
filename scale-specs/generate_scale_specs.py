093b6aae2e6f7756a715863350f152a49d191ebc
Alexander Ott <66271487+AlexanderHott@users.noreply.github.com>
2025-06-23 20:36:03 -0400

    add complex specs

--- scale-specs/generate_scale_specs.py
+++ scale-specs/generate_scale_specs.py
@@ -4,6 +4,14 @@ i_pct = 0.80
 u_pct = 0.10
 pq_pct = 0.10
 
+
+complex_i_pct = 0.35
+complex_pq_pct = 0.40
+complex_epq_pct = 0.10
+complex_rq_pct = 0.10
+complex_rd_pct = 0.04
+complex_pd_pct = 0.01
+
 baselines = [
     1_000_000,
     5_000_000,
@@ -14,8 +22,9 @@ baselines = [
 
 for baseline in baselines:
     scale_str = f"{baseline // 1_000_000}".zfill(3)
-    filename = f"{scale_str}m.spec.json"
-    spec = {
+    filename_simple = f"{scale_str}m.spec.json"
+    filename_complex = f"{scale_str}m-complex.spec.json"
+    spec_simple = {
         "$schema": "../workload_schema.json",
         "sections": [
             {
@@ -43,5 +52,47 @@ for baseline in baselines:
             }
         ],
     }
-    with open(filename, "w") as f:
-        json.dump(spec, f, indent=2)
+
+    spec_complex = {
+        "$schema": "../workload_schema.json",
+        "sections": [
+            {
+                "groups": [
+                    {
+                        "inserts": {
+                            "amount": baseline,
+                            "key": {"uniform": {"len": 32, "character_set": "numeric"}},
+                            "val": {"uniform": {"len": 992}},
+                        }
+                    },
+                    {
+                        "inserts": {
+                            "amount": round(baseline * complex_i_pct),
+                            "key": {"uniform": {"len": 32, "character_set": "numeric"}},
+                            "val": {"uniform": {"len": 992}},
+                        },
+                        "point_queries": {"amount": round(baseline * complex_pq_pct)},
+                        "empty_point_queries": {
+                            "amount": round(baseline * complex_epq_pct),
+                            "key": {"uniform": {"len": 32, "character_set": "numeric"}},
+                        },
+                        "range_queries": {
+                            "amount": round(baseline * complex_rq_pct),
+                            "selectivity": 0.1,
+                        },
+                        "point_deletes": {
+                            "amount": round(baseline * complex_pd_pct),
+                        },
+                        "range_deletes": {
+                            "amount": round(baseline * complex_rd_pct),
+                            "selectivity": 0.01,
+                        },
+                    },
+                ]
+            }
+        ],
+    }
+    with open(filename_simple, "w") as f:
+        json.dump(spec_simple, f, indent=2)
+    with open(filename_complex, "w") as f:
+        json.dump(spec_complex, f, indent=2)
