f6bd5292b64bc2f12a46fab59b8dcc51d4228832
Alex Ott <66271487+AlexanderHott@users.noreply.github.com>
2025-08-31 05:52:01 -0400

    cleanup repo

--- bench_ycsb.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-hyperfine \
-  --prepare "cargo build --release --target-dir target" \
-  --parameter-list spec a,b,c,d,e,f \
-  "target/release/tectonic-cli generate -w ./ycsb-specs/{spec}.spec.json" \
-  --export-json ycsb.json
